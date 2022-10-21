d55797418762942bb39cfd22a904fff14a2ad78e
JaberHQ <jaber4hmed@gmail.com>
2022-10-21 16:04:02 +0100

    Initial commit

---
+++ Source/PizzaSim/PizzaSimGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PizzaSimGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PIZZASIM_API APizzaSimGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
